f5e18fd23a1997c2d3ebaa2cacbeee3b269237ea
Lassemon <lassemon.pirinen@gmail.com>
2015-08-04 13:21:39 +0300

    Add animated transition to stickymenu

--- functions.php
+++ functions.php
@@ -33,6 +33,7 @@ function custom_enqueue_styles(){
 add_action( 'wp_enqueue_scripts', 'custom_enqueue_styles', 15 );
 
 function scripts(){
+	wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/js/modernizr.js', array(), '20151601', true);
 	if(is_front_page()){
 		wp_enqueue_script('frontpage-script', get_stylesheet_directory_uri() . '/js/stickymenu.js', array('jquery'), '20151601', true);
 	}
--- js/stickymenu.js
+++ js/stickymenu.js
@@ -1,9 +1,32 @@
 jQuery(document).ready(function($) {  
  	"use strict"
 	
+	/* STICKY NAVIGATION */
 	var menuId = '.menu-stickymenu-container';
+	var stickyMenu = $(menuId);
 	var showMenuLabel = $(menuId +' .show-menu');
 	var stickyNavTop = $(menuId).offset().top; 
+	var sectionOffsets = [];
+
+	var initSectionOffsets = function(){
+		$('.content-area > div').each(function(index, value){
+			sectionOffsets[index] = $(value).offset().top;
+		});
+	};
+
+	$( window ).resize(function() {
+		initSectionOffsets();
+	});
+	initSectionOffsets();
+
+
+	if(location.hash){
+		stickyMenu.find('a[href='+location.hash+']').addClass('selected');
+	} else {
+		stickyMenu.find('a:first').addClass('selected');
+		
+	}
+	
 
 	$(window).scroll(function(){  
 		var scrollTop = $(window).scrollTop();  
@@ -14,4 +37,44 @@ jQuery(document).ready(function($) {
 			$(menuId).removeClass('sticky-header');   
 		}  
 	});
+
+	/* Scroll to specific section on front page */
+	$(function() {
+		stickyMenu.find('.menu a[href*=#]:not([href=#])').click(function(event) {
+			event.preventDefault();
+
+			var clickedLink = $(this);
+			var clickedIndex = clickedLink.parent().index();
+
+			if(showMenuLabel.is(':visible')) {
+				showMenuLabel.click();
+			}
+
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('.menu [name=' + this.hash.slice(1) +']');
+
+				if(Modernizr.history) {
+					history.pushState(null, null, this.hash);
+				}
+
+				if (target.length && clickedIndex >= 0) {
+					$('.content-area').animate({
+						scrollTop: sectionOffsets[clickedIndex]
+					}, 500);
+				}
+
+				clickedLink.parent().siblings().find("a").removeClass("selected");
+				clickedLink.addClass("selected");
+
+			}
+
+		});
+	});
+
+	jQuery.easing.easeOutQuart = function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	};
+	/* END OF STICKY NAVIGATION */
+
 });
